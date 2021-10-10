9020504203fe9c51a983f540f6039827737d4055
Ilya Motkin <xresou@gmail.com>
2021-10-10 18:18:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const bodyParser = require('body-parser');
-
+const PORT = process.env.PORT || 5000; 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-app.set("port", 5000);
-
 app.get("/", (req, res) => {
     res.sendFile('./html/main.html', {root: "./"});
   });
 
-app.listen(app.get("port"), () => {
-  console.log(`Server running at http://localhost:${app.get("port")}`);
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
 
